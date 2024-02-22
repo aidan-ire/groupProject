b3678abeb5a18c26255da0611b12986234fed3d0
Aidan Hughes <aidan@Aidans-MacBook-Pro.local>
2024-02-22 10:52:16 +0000

    Add untracked files

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.static('views'));
+app.use(express.static('style'));
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
